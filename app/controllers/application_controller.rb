19a61bf933e3c4f2faec4adca7e485c86c77e1d1
Riley Moore <rilemoore@gmail.com>
2019-11-29 23:28:58 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,51 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+  get '/articles/new' do
+    erb :new
+  end
+  
+  post '/articles' do
+    Article.create(params)
+    redirection = '/articles/' + Article.find(Article.all.last.id).id.to_s
+    redirect redirection
+  end
+  
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find(params["id"])
+    erb :show
+  end
+  
+  get '/articles/:id/edit' do
+    @article = Article.find(params["id"])
+    erb :edit
+  end
+  
+  patch '/articles/:id' do
+    new_info = {}
+    article = Article.find(params["id"])
+    new_info[:title] = params["title"]
+    new_info[:content] = params["content"]
+    article.update(new_info)
+    
+    redirect "/articles/#{params["id"]}"
+    
+  end
+  
+  
+  delete '/articles/:id/delete' do
+    @article = Article.find(params["id"])
+    @article.destroy
+    redirect "/articles"
+  end
+  
+  
+  
+  
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,12 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+  
+  # attr_accessor :title, :content
+  
+  # def initialize(title, content)
+  #   @title = title
+  #   @content = content
+  # end
+  
 end
